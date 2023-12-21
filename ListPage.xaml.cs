7a4e7182722383962d46dbef7960f1577897b6d0
Miruna <miruna.crintea@stud.ubbcluj.ro>
2023-12-21 02:12:52 +0200

    Laborator 9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -21,4 +21,35 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        Product p;
+        if (listView.SelectedItem != null)
+        {
+            p = listView.SelectedItem as Product;
+            await App.Database.DeleteListProductAsync(slist.ID, p.ID);
+            listView.ItemsSource = await App.Database.GetListProductsAsync(slist.ID);
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
